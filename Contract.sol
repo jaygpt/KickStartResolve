2a90c19c091841015dd94c4602c90026892d678e
Jay <jaygpt@iitk.ac.in>
2018-07-04 19:28:08 +0000

    Corrected Storrage Error

--- Contract.sol
+++ Contract.sol
@@ -1,10 +1,23 @@
 pragma solidity ^0.4.17;
 
 contract Campaign {
+    struct Request{
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+    }
+    
+    Request[] public requests;
     address public manager;
     uint public minimumContribution;
     address[] public approvers;
-
+    
+    modifier restricted(){
+        require(msg.sender == manager);
+        _;
+    }
+    
     constructor (uint minimum) public {
         manager = msg.sender;
         minimumContribution = minimum;
@@ -15,4 +28,17 @@ contract Campaign {
 
         approvers.push(msg.sender);
     }
+    
+    function createRequest(string description,uint value,address recipient) public restricted{
+        Request memory newRequest = Request({
+            description: description,
+            value: value,
+            recipient: recipient,
+            complete: false
+        });
+        
+        requests.push(newRequest);
+    }
+    
+    
 }
