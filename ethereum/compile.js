fbb74817f5c9174710c4fefc7f8ed5cd88303176
Jay <jaygpt@iitk.ac.in>
2018-07-08 23:31:49 +0000

    compiled to build without error due to constructor in contract

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -1,19 +1,20 @@
-const path = require('path');
-const fs = require('fs-extra');
-const solc = require('solc');
 
-const buildPath = path.resolve(__dirname,'build');
+const path = require('path');
+const solc = require('solc');
+const fs = require('fs-extra');
+
+const buildPath = path.resolve(__dirname, 'build');
 fs.removeSync(buildPath);
 
-const campaignPath = path.resolve(__dirname,'contracts','Campiagn.sol');
-const source = fs.readFileSync(campaignPath,'utf8');
-
-const output = solc.compile(source,1).contracts;
-
+const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
+const source = fs.readFileSync(campaignPath, 'utf8');
+const output = solc.compile(source, 1).contracts;
+console.log(output);
 fs.ensureDirSync(buildPath);
-for(let contracts in output){
-    fs.outputJsonSync(
-        path.resolve(buildPath,contracts + '.json'),
-        output[contracts]
-    );
+
+for (let contract in output) {
+  fs.outputJsonSync(
+    path.resolve(buildPath, contract.replace(':', '') + '.json'),
+    output[contract]
+  );
 }
