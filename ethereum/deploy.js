1ea053db9f3d8b2cea0a6034bfc87faa56191f52
Jay <jaygpt@iitk.ac.in>
2018-07-09 22:57:30 +0000

    deployed contract

---
+++ ethereum/deploy.js
@@ -0,0 +1,19 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const compileFactory = require('./build/CampaignFactory.json');
+//interface is abi key which interacts with js and solidity whereas bytecode is overall contract
+const provider = new HDWalletProvider('melt garment circle duck rude report argue tongue gate harbor carpet nephew','https://rinkeby.infura.io/IwXeaA7paHZsIDAMIwMw');
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from', accounts[0]);
+
+    const results = await new web3.eth.Contract(JSON.parse(compileFactory.interface))
+        .deploy({ data: compileFactory.bytecode })
+        .send({ gas: 1000000,from: accounts[0]})
+    console.log('contract deployed to ', results.options.address);
+};
+deploy();
+
