263e0b5bbe9d6009c08e2a4150a1da6d01208406
Jay <jaygpt@iitk.ac.in>
2018-07-13 00:47:24 +0000

    redeployed contract

--- .next/dist/pages/campaign/show.js
+++ .next/dist/pages/campaign/show.js
@@ -4,6 +4,14 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
+var _regenerator = require('babel-runtime/regenerator');
+
+var _regenerator2 = _interopRequireDefault(_regenerator);
+
+var _asyncToGenerator2 = require('babel-runtime/helpers/asyncToGenerator');
+
+var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
+
 var _getPrototypeOf = require('babel-runtime/core-js/object/get-prototype-of');
 
 var _getPrototypeOf2 = _interopRequireDefault(_getPrototypeOf);
@@ -32,6 +40,10 @@ var _layout = require('../../components/layout');
 
 var _layout2 = _interopRequireDefault(_layout);
 
+var _Campaign = require('../../ethereum/Campaign');
+
+var _Campaign2 = _interopRequireDefault(_Campaign);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var _jsxFileName = '/home/jay/Desktop/new/KickStart/pages/campaign/show.js?entry';
@@ -52,19 +64,55 @@ var CampaignShow = function (_Component) {
             return _react2.default.createElement(_layout2.default, {
                 __source: {
                     fileName: _jsxFileName,
-                    lineNumber: 7
+                    lineNumber: 18
                 }
             }, _react2.default.createElement('h1', {
                 __source: {
                     fileName: _jsxFileName,
-                    lineNumber: 8
+                    lineNumber: 19
                 }
             }, ' Campaign is Here!!!aa '));
         }
+    }], [{
+        key: 'getInitialProps',
+
+        //getInitialProps methopd is used to fetch initial data from browser and other places
+
+        value: function () {
+            var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(props) {
+                var campaign, summary;
+                return _regenerator2.default.wrap(function _callee$(_context) {
+                    while (1) {
+                        switch (_context.prev = _context.next) {
+                            case 0:
+                                campaign = (0, _Campaign2.default)(props.query.address);
+                                _context.next = 3;
+                                return campaign.methods.getSummary().call();
+
+                            case 3:
+                                summary = _context.sent;
+
+                                console.log(summary);
+                                return _context.abrupt('return', {});
+
+                            case 6:
+                            case 'end':
+                                return _context.stop();
+                        }
+                    }
+                }, _callee, this);
+            }));
+
+            function getInitialProps(_x) {
+                return _ref.apply(this, arguments);
+            }
+
+            return getInitialProps;
+        }()
     }]);
 
     return CampaignShow;
 }(_react.Component);
 
 exports.default = CampaignShow;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2NhbXBhaWduL3Nob3cuanMiXSwibmFtZXMiOlsiUmVhY3QiLCJDb21wb25lbnQiLCJMYXlvdXQiLCJDYW1wYWlnblNob3ciXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7O0FBQUEsQUFBTyxBQUFTOzs7O0FBQ2hCLEFBQU8sQUFBWTs7Ozs7Ozs7O0lBRWIsQTs7Ozs7Ozs7Ozs7aUNBQ00sQUFDSjttQ0FDSSxBQUFDOzs4QkFBRDtnQ0FBQSxBQUNBO0FBREE7QUFBQSxhQUFBLGtCQUNBLGNBQUE7OzhCQUFBO2dDQUFBO0FBQUE7QUFBQSxlQUZKLEFBQ0ksQUFDQSxBQUlYOzs7OztBQVIwQixBLEFBVzNCOztrQkFBQSxBQUFlIiwiZmlsZSI6InNob3cuanM/ZW50cnkiLCJzb3VyY2VSb290IjoiL2hvbWUvamF5L0Rlc2t0b3AvbmV3L0tpY2tTdGFydCJ9
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2NhbXBhaWduL3Nob3cuanMiXSwibmFtZXMiOlsiUmVhY3QiLCJDb21wb25lbnQiLCJMYXlvdXQiLCJDYW1wYWlnbiIsIkNhbXBhaWduU2hvdyIsInByb3BzIiwiY2FtcGFpZ24iLCJxdWVyeSIsImFkZHJlc3MiLCJtZXRob2RzIiwiZ2V0U3VtbWFyeSIsImNhbGwiLCJzdW1tYXJ5IiwiY29uc29sZSIsImxvZyJdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7OztBQUFBLEFBQU8sQUFBUzs7OztBQUNoQixBQUFPLEFBQVk7Ozs7QUFDbkIsQUFBTyxBQUFjOzs7Ozs7Ozs7SUFDZixBOzs7Ozs7Ozs7OztpQ0FZTSxBQUNKO21DQUNJLEFBQUM7OzhCQUFEO2dDQUFBLEFBQ0E7QUFEQTtBQUFBLGFBQUEsa0JBQ0EsY0FBQTs7OEJBQUE7Z0NBQUE7QUFBQTtBQUFBLGVBRkosQUFDSSxBQUNBLEFBSVg7OzthQWxCRzs7Ozs7aUgsQUFFNkI7Ozs7O2lDQUNuQjtBLDJDQUFXLHdCQUFTLE1BQUEsQUFBTSxNQUFmLEEsQUFBcUI7O3VDQUVoQixTQUFBLEFBQVMsUUFBVCxBQUFpQixhQUFqQixBLEFBQThCOztpQ0FBOUM7QSxtREFFTjs7d0NBQUEsQUFBUSxJQUFSLEFBQVk7aUVBQ0wsQTs7Ozs7Ozs7Ozs7Ozs7Ozs7OztBQVRZLEEsQUFzQjNCOztrQkFBQSxBQUFlIiwiZmlsZSI6InNob3cuanM/ZW50cnkiLCJzb3VyY2VSb290IjoiL2hvbWUvamF5L0Rlc2t0b3AvbmV3L0tpY2tTdGFydCJ9
--- pages/campaign/show.js
+++ pages/campaign/show.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import Layout from '../../components/layout';
-
+import Campaign from '../../ethereum/Campaign';
 class CampaignShow extends Component{
+    //getInitialProps methopd is used to fetch initial data from browser and other places
+
+    static async getInitialProps(props){
+        const campaign = Campaign(props.query.address);
+
+        const summary = await campaign.methods.getSummary().call();
+
+        console.log(summary);
+        return {};
+    }
+
     render(){
         return (
             <Layout>
