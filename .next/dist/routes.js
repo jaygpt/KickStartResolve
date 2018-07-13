d9354c8724e0ba22145ef2f6baa583f107c6ac95
Jay <jaygpt@iitk.ac.in>
2018-07-13 23:51:57 +0000

    Added Contribute Form without error

--- .next/dist/routes.js
+++ .next/dist/routes.js
@@ -2,8 +2,7 @@
 
 var routes = require('next-routes')();
 
-routes.add('/campaign/new', '/campaign/new');
-routes.add('/campaign/:address', '/campaign/show');
+routes.add('/campaign/new', '/campaign/new').add('/campaign/:address', '/campaign/show');
 
 module.exports = routes;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInJvdXRlcy5qcyJdLCJuYW1lcyI6WyJyb3V0ZXMiLCJyZXF1aXJlIiwiYWRkIiwibW9kdWxlIiwiZXhwb3J0cyJdLCJtYXBwaW5ncyI6Ijs7QUFBQSxJQUFNLFNBQVMsQUFBZjs7QUFFQSxPQUFPLEFBQVAsSUFBVyxBQUFYLGlCQUEyQixBQUEzQjtBQUNBLE9BQU8sQUFBUCxJQUFXLEFBQVgsc0JBQWlDLEFBQWpDOztBQUVBLE9BQU8sQUFBUCxVQUFpQixBQUFqQiIsImZpbGUiOiJyb3V0ZXMuanMiLCJzb3VyY2VSb290IjoiL2hvbWUvamF5L0Rlc2t0b3AvbmV3L0tpY2tTdGFydCJ9
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInJvdXRlcy5qcyJdLCJuYW1lcyI6WyJyb3V0ZXMiLCJyZXF1aXJlIiwiYWRkIiwibW9kdWxlIiwiZXhwb3J0cyJdLCJtYXBwaW5ncyI6Ijs7QUFBQSxJQUFNLFNBQVMsQUFBZjs7QUFFQSxPQUNLLEFBREwsSUFDUyxBQURULGlCQUN5QixBQUR6QixpQkFFSyxBQUZMLElBRVMsQUFGVCxzQkFFK0IsQUFGL0I7O0FBSUEsT0FBTyxBQUFQLFVBQWlCLEFBQWpCIiwiZmlsZSI6InJvdXRlcy5qcyIsInNvdXJjZVJvb3QiOiIvaG9tZS9qYXkvRGVza3RvcC9uZXcvS2lja1N0YXJ0In0=
--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const routes = require('next-routes')();
 
-routes.add('/campaign/new','/campaign/new');
-routes.add('/campaign/:address', '/campaign/show');
+routes
+    .add('/campaign/new','/campaign/new')
+    .add('/campaign/:address', '/campaign/show');
 
 module.exports = routes;
